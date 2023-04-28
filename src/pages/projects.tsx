0aed3bf2d1df70223fcee4c7d9d302514e466cca
Sanseli <lisasansen97@gmail.com>
2023-04-28 16:47:42 +0300

    add scroll animations

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -13,42 +13,20 @@ export default function Home() {
         {
             id: 2,
             title: 'Snellehap Website',
-            description: 'Small singlepage website for sandwich bar',
+            description: 'Small singlepage website for sandwich bar.',
             link: 'https://snellehap.be/',
             tools: ['next', 'tailwind']
         },
         {
-            id: 1,
-            title: 'The Beehive Website',
-            description: 'Company website with dynamic blog and job components. Transformed the design made in Figma into the current company website.',
-            link: 'https://thebeehive.be/',
-            tools: ['nuxt', 'tailwind', 'strapi', 'figma']
-        },
-        {
-            id: 1,
-            title: 'The Beehive Website',
-            description: 'Company website with dynamic blog and job components. Transformed the design made in Figma into the current company website.',
-            link: 'https://thebeehive.be/',
-            tools: ['nuxt', 'tailwind', 'strapi', 'figma']
-        },
-        {
-            id: 1,
-            title: 'The Beehive Website',
-            description: 'Company website with dynamic blog and job components. Transformed the design made in Figma into the current company website.',
-            link: 'https://thebeehive.be/',
-            tools: ['nuxt', 'tailwind', 'strapi', 'figma']
-        },
-        {
-            id: 1,
-            title: 'The Beehive Website',
-            description: 'Company website with dynamic blog and job components. Transformed the design made in Figma into the current company website.',
-            link: 'https://thebeehive.be/',
-            tools: ['nuxt', 'tailwind', 'strapi', 'figma']
-        },
+            id: 3,
+            title: 'Crescendo 2',
+            description: '',
+            tools: ['nuxt', 'vue', 'tailwind', 'jest', 'laravel']
+        }
     ]
 
     return (
-        <main className="grid grid-cols-2 overflow-auto">
+        <main className="overflow-auto scroll-smooth snap-y snap-mandatory">
             {projects.map(project => <Project project={project} key={project.id} />)}
         </main>
     )
